939ab9334c03bb2df0b72d3e920fe4572cdce95c
Diallo <ibrahimasory22@hotmail.com>
2024-03-03 23:57:33 +0100

    Update main.py

--- Ex2/main.py
+++ Ex2/main.py
@@ -3,7 +3,7 @@ from Shape import Square,Triangle
 
 def main():
     my_triangle = Triangle(a=3,b=4,c=5)
-    print(f"Triangle perimeter is "
+    print(f"Triangle perimeter is is "
     f"{my_triangle.perimeter()}")
     my_square = Square(3)
     print(f"My square area iiiis  {my_square.area()}")
